3f6ba81cf75281166fe925a71df6d60498fe7abe
Jakob Peters <jakobj.peters@gmail.com>
2024-02-04 18:12:05 -0800

    Implement an update button and FPS counter

--- gui.py
+++ gui.py
@@ -1,18 +1,21 @@
 import tkinter as tk
+import time
+from main import World
+
+WIDTH = 800
+HEIGHT = 600
+FPS_REFRESH_RATE = 1 # second
 
 class App:
-    def __init__(self, root, new_world):
+    def __init__(self, root):
         self.root = root
-        self.world = new_world
         root.title("A-Life Challenge")
 
         # window size
-        width = 800
-        height = 600
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height,
-                                    (screenwidth - width) / 2, (screenheight - height) / 2)
+        alignstr = '%dx%d+%d+%d' % (WIDTH, HEIGHT,
+                                    (screenwidth - WIDTH) / 2, (screenheight - HEIGHT) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
@@ -22,8 +25,8 @@ class App:
             self.main_frame, width=250, height=200, bg='#3E3E3E')
 
         # dynamic coordinates
-        canvas_center_x = width / 2
-        canvas_center_y = height / 2
+        canvas_center_x = WIDTH / 2
+        canvas_center_y = HEIGHT / 2
 
         # canvas for buttons
         button_canvas = tk.Canvas(
@@ -59,6 +62,8 @@ class App:
         self.main_frame.pack(fill=tk.BOTH, expand=True)
 
     def start_button_command(self):
+        self.world = World()
+
         # new frame for after pushing the start button
         start_screen_frame = tk.Frame(
             self.main_frame, width=800, height=600, bg='#ffffff')
@@ -66,16 +71,13 @@ class App:
                            height=600, bg='#2f2f2f')
         canvas.pack()
 
-        canvas.create_text(550, 30, text="Live information will go here",
-                           font=('Times', 16), fill="blue")
-        canvas.create_text(550, 50, text="Live information will go here",
-                           font=('Times', 16), fill="red")
-        canvas.create_text(550, 70, text="Live information will go here",
-                           font=('Times', 16), fill="orange")
-        canvas.create_text(550, 90, text="Live information will go here",
-                           font=('Times', 16), fill="green")
-        canvas.create_text(550, 110, text="Live information will go here",
-                           font=('Times', 16), fill="white")
+        update_button = tk.Button(canvas, text="Update", command=self.update_button_command,
+                        width=30, height=2, bg="#5189f0", fg="#FFFFFF", activebackground="#5C89f0")
+        update_button.place(relx=0.8, rely=0.5, anchor=tk.CENTER)
+
+        for i, color in enumerate(("blue", "red", "orange", "green", "white")):
+            canvas.create_text(550, 20 * i + 30, text="Live information will go here",
+                font=('Times', 16), fill=color)
 
         # text display for the world state
         self.output_box = tk.Text(self.main_frame, wrap=tk.WORD, width=50,
@@ -86,6 +88,11 @@ class App:
         self.update_output_box()
         self.show_screen(start_screen_frame)
 
+    def update_button_command(self):
+        print("update")
+        self.world.update()
+        self.render()
+
     def load_button_command(self):
         print("Load button command")
 
@@ -95,13 +102,27 @@ class App:
     def update_output_box(self):
         # clear the output box
         self.output_box.delete(1.0, tk.END)
+        self.render()
 
-        # display the ascii board
-        world_state = str(self.world)
-        self.output_box.insert(tk.END, world_state)
+    def render(self):
+        self.output_box.delete(1.0, tk.END)
+        self.output_box.insert(tk.END, str(self.world))
 
-
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
-    root.mainloop()"""
+    
+    start, counter = time.time(), 0
+
+    while True:
+        root.update_idletasks()
+        root.update()
+
+        counter += 1
+        current = time.time()
+        elapsed = current - start
+        if elapsed > FPS_REFRESH_RATE:
+            print("FPS: ", counter / elapsed)
+            counter = 0
+            start = current
+
